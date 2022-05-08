6c96d6e4094f05ab98df4c9c45f10a865f12e06f
Radmila Gama <milaejared@outlook.com>
2022-05-08 12:05:37 -0300

    parte 1 concluida

--- Lista 1/Questao1-9.py
+++ Lista 1/Questao1-9.py
@@ -1,4 +1,43 @@
 print("1. Escreva a expressão 1 + 2 + 3 e verifique que o resultado é 6.")
 print(1+2+3)
-print("Escreva a expressão 10 + 5 ∗ 2 e verifique o resultado. Entenda como este valor é gerado.")
+
+print("2. Escreva a expressão 10 + 5 ∗ 2 e verifique o resultado. Entenda como este valor é gerado.")
 print(10+5*2)
+
+print("3.Escreva a expressão 10 + 100%8 e verifique o resultado.")
+print(10 + 100%8)
+
+print("4. Escreva a expressão 10 + 100/8 e verifique o resultado.")
+print(10+100/8)
+
+print("5. Escreva a expressão 10 + 100//8 e verifique o resultado.")
+print(10 + 100//8)
+
+print("6. Escreva a seguinte sequencia de instruções:")
+print("Depois use o comando print() para cada uma das variáveis e entenda o valor de cada uma delas")
+x=10
+y=20
+w=x+y
+x=w+1.0
+y=x/2
+z=x//2
+print("w=", w, "x=", x, "y=", y, "z =", z)
+
+print("7. A expressão a seguir deveria calcular a média aritmética entre 7 e 9.5: 7+9.5/2. O resultado não é o esperado. Isso acontece porque a divisão é realizada antes de soma. Para resolver este problema deve-se colocar em evidência a soma. Em Python3 usa-se os parênteses. A expressão correta é (7+9.5)/2. Verifique e confirme.")
+print("7+9.5/2=", 7+9.5/2)
+print("(7+9.5)/2 =", (7+9.5)/2)
+
+print("8. Escreva a sequencia de atribuições a seguir:")
+x=0.1
+x=x+0.1
+x=x+0.1
+x=x+0.1
+x=x+0.1
+x=x+0.1
+x=x+0.1
+x=x+0.1
+x=x+0.1
+x=x+0.1
+print(x)
+print("Verifique o valor de x mostrado com o comando print(x). Você consegue explicar o resultado?")
+print("O resutado apresentado é 0.9999999999999999, pois, apesar de x começar com o valor 0.1, ao longo da expressão, seu valor anterior é somado com 0.1, gerando um novo valor")
