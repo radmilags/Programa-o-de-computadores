e721e71a1263dec79222939a4512bc54fe43fecf
Radmila Gama <milaejared@outlook.com>
2022-05-08 11:55:33 -0300

    ok

--- Lista 1/Questao1-9.py
+++ Lista 1/Questao1-9.py
@@ -1,2 +1,4 @@
 print("1. Escreva a expressão 1 + 2 + 3 e verifique que o resultado é 6.")
 print(1+2+3)
+print("Escreva a expressão 10 + 5 ∗ 2 e verifique o resultado. Entenda como este valor é gerado.")
+print(10+5*2)
